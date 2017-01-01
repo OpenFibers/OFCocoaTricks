1a2a3b5372f86a6c5a52b868f7c3a7dde6b1fb9f
OpenFibers <openfibers@gmail.com>
2017-01-02 02:40:29 +0800

    Initial Commit

---
+++ OFCocoaTricks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OFCocoaTricks
+//
+//  Created by openthread on 02/01/2017.
+//  Copyright © 2017 openfibers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
