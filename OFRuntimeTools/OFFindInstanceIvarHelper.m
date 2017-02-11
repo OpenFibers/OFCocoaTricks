03dc89b36a7a53bd9bdbd9f7b15a2d1b14f56bad
OpenFibers <openfibers@gmail.com>
2017-02-12 02:21:42 +0800

    alse find ivar in super class declaration

--- OFRuntimeTools/OFFindInstanceIvarHelper.m
+++ OFRuntimeTools/OFFindInstanceIvarHelper.m
@@ -69,107 +69,113 @@
     
     NSMutableArray <OFFindInstanceIvarHelperRelationshipObject *> *resultArray = [NSMutableArray array];
     
-    unsigned int count;
-    Ivar *vars = class_copyIvarList([superObject class], &count);
-    for (NSUInteger i = 0; i < count; i++)
+    Class tempClass = [superObject class];
+    while (tempClass)
     {
-        Ivar var = vars[i];
-        const char *type = ivar_getTypeEncoding(var);
-        
-        if (type[0] != @encode(id)[0])
+        unsigned int count;
+        Ivar *vars = class_copyIvarList(tempClass, &count);
+        for (NSUInteger i = 0; i < count; i++)
         {
-            continue;
-        }
-        
-        ptrdiff_t offset = ivar_getOffset(var);
-        uintptr_t *pointer = (__bridge void *)superObject + offset;
-        if (*pointer == (uintptr_t)(__bridge void *)object)
-        {
-            OFFindInstanceIvarHelperRelationshipObject *r;
-            r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"EQUAL_TO"
-                                                                  superObjectClass:[superObject class]
-                                                                superObjectPointer:(uintptr_t)superObject
-                                                                          ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
-                                                                          ivarType:[NSString stringWithUTF8String:type]
-                                                               ivarDeclaredInClass:[superObject class]];
-            [resultArray addObject:r];
-            continue;
-        }
-        
-        id value = [OFRuntimeUltilities valueForIvar:var onObject:superObject];
-        if ([object isKindOfClass:[UIView class]] && [value isKindOfClass:[UIView class]])
-        {
-            UIView *viewValue = value;
-            if ([viewValue.subviews containsObject:(UIView *)object])
+            Ivar var = vars[i];
+            const char *type = ivar_getTypeEncoding(var);
+            
+            if (type[0] != @encode(id)[0])
+            {
+                continue;
+            }
+            
+            ptrdiff_t offset = ivar_getOffset(var);
+            uintptr_t *pointer = (__bridge void *)superObject + offset;
+            if (*pointer == (uintptr_t)(__bridge void *)object)
             {
                 OFFindInstanceIvarHelperRelationshipObject *r;
-                r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"SUBVIEW_OF"
+                r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"EQUAL_TO"
                                                                       superObjectClass:[superObject class]
                                                                     superObjectPointer:(uintptr_t)superObject
                                                                               ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
                                                                               ivarType:[NSString stringWithUTF8String:type]
-                                                                   ivarDeclaredInClass:[superObject class]];
+                                                                   ivarDeclaredInClass:tempClass];
                 [resultArray addObject:r];
+                continue;
             }
-            else if ([((UIView *)object) isDescendantOfView:viewValue])
+            
+            id value = [OFRuntimeUltilities valueForIvar:var onObject:superObject];
+            if ([object isKindOfClass:[UIView class]] && [value isKindOfClass:[UIView class]])
             {
-                OFFindInstanceIvarHelperRelationshipObject *r;
-                r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"DESCENDANTVIEW_OF"
-                                                                      superObjectClass:[superObject class]
-                                                                    superObjectPointer:(uintptr_t)superObject
-                                                                              ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
-                                                                              ivarType:[NSString stringWithUTF8String:type]
-                                                                   ivarDeclaredInClass:[superObject class]];
-                [resultArray addObject:r];
-            }
-            continue;
-        }
-        
-        if ([value respondsToSelector:@selector(objectEnumerator)])
-        {
-            NSEnumerator *enumerator = [value objectEnumerator];
-            if ([enumerator isKindOfClass:[NSEnumerator class]])
-            {
-                id element = nil;
-                while ((element = [enumerator nextObject]))
+                UIView *viewValue = value;
+                if ([viewValue.subviews containsObject:(UIView *)object])
                 {
-                    if (element == object)
+                    OFFindInstanceIvarHelperRelationshipObject *r;
+                    r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"SUBVIEW_OF"
+                                                                          superObjectClass:[superObject class]
+                                                                        superObjectPointer:(uintptr_t)superObject
+                                                                                  ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
+                                                                                  ivarType:[NSString stringWithUTF8String:type]
+                                                                       ivarDeclaredInClass:tempClass];
+                    [resultArray addObject:r];
+                }
+                else if ([((UIView *)object) isDescendantOfView:viewValue])
+                {
+                    OFFindInstanceIvarHelperRelationshipObject *r;
+                    r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"DESCENDANTVIEW_OF"
+                                                                          superObjectClass:[superObject class]
+                                                                        superObjectPointer:(uintptr_t)superObject
+                                                                                  ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
+                                                                                  ivarType:[NSString stringWithUTF8String:type]
+                                                                       ivarDeclaredInClass:tempClass];
+                    [resultArray addObject:r];
+                }
+                continue;
+            }
+            
+            if ([value respondsToSelector:@selector(objectEnumerator)])
+            {
+                NSEnumerator *enumerator = [value objectEnumerator];
+                if ([enumerator isKindOfClass:[NSEnumerator class]])
+                {
+                    id element = nil;
+                    while ((element = [enumerator nextObject]))
                     {
-                        OFFindInstanceIvarHelperRelationshipObject *r;
-                        r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"VALUE_OF"
-                                                                              superObjectClass:[superObject class]
-                                                                            superObjectPointer:(uintptr_t)superObject
-                                                                                      ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
-                                                                                      ivarType:[NSString stringWithUTF8String:type]
-                                                                           ivarDeclaredInClass:[superObject class]];
-                        [resultArray addObject:r];
+                        if (element == object)
+                        {
+                            OFFindInstanceIvarHelperRelationshipObject *r;
+                            r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"VALUE_OF"
+                                                                                  superObjectClass:[superObject class]
+                                                                                superObjectPointer:(uintptr_t)superObject
+                                                                                          ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
+                                                                                          ivarType:[NSString stringWithUTF8String:type]
+                                                                               ivarDeclaredInClass:tempClass];
+                            [resultArray addObject:r];
+                        }
+                    }
+                }
+            }
+            
+            if ([value respondsToSelector:@selector(keyEnumerator)])
+            {
+                NSEnumerator *enumerator = [value keyEnumerator];
+                if ([enumerator isKindOfClass:[NSEnumerator class]])
+                {
+                    id element = nil;
+                    while ((element = [enumerator nextObject]))
+                    {
+                        if (element == object)
+                        {
+                            OFFindInstanceIvarHelperRelationshipObject *r;
+                            r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"KEY_OF"
+                                                                                  superObjectClass:[superObject class]
+                                                                                superObjectPointer:(uintptr_t)superObject
+                                                                                          ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
+                                                                                          ivarType:[NSString stringWithUTF8String:type]
+                                                                               ivarDeclaredInClass:tempClass];
+                            [resultArray addObject:r];
+                        }
                     }
                 }
             }
         }
         
-        if ([value respondsToSelector:@selector(keyEnumerator)])
-        {
-            NSEnumerator *enumerator = [value keyEnumerator];
-            if ([enumerator isKindOfClass:[NSEnumerator class]])
-            {
-                id element = nil;
-                while ((element = [enumerator nextObject]))
-                {
-                    if (element == object)
-                    {
-                        OFFindInstanceIvarHelperRelationshipObject *r;
-                        r = [OFFindInstanceIvarHelperRelationshipObject objectWithRelationShip:@"KEY_OF"
-                                                                              superObjectClass:[superObject class]
-                                                                            superObjectPointer:(uintptr_t)superObject
-                                                                                      ivarName:[NSString stringWithUTF8String:ivar_getName(var)]
-                                                                                      ivarType:[NSString stringWithUTF8String:type]
-                                                                           ivarDeclaredInClass:[superObject class]];
-                        [resultArray addObject:r];
-                    }
-                }
-            }
-        }
+        tempClass = class_getSuperclass(tempClass);
     }
     
     return [NSArray arrayWithArray:resultArray];
