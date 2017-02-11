254882a3fd99f85c979ac8d35d1c59b36ae7d316
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2017-02-12 01:51:50 +0800

    make OFFindInstanceIvarHelperRelationshipObject public

--- OFRuntimeTools/OFFindInstanceIvarHelper.h
+++ OFRuntimeTools/OFFindInstanceIvarHelper.h
@@ -9,6 +9,15 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@interface OFFindInstanceIvarHelperRelationshipObject : NSObject
+@property (nonatomic, strong) NSString *relationShip;
+@property (nonatomic, strong) Class superObjectClass;
+@property (nonatomic, assign) uintptr_t superObjectPointer;
+@property (nonatomic, strong) NSString *ivarName;
+@property (nonatomic, strong) NSString *ivarType;
+@property (nonatomic, strong) Class ivarDeclaredInClass;
+@end
+
 @interface OFFindInstanceIvarHelper : NSObject
 
 + (NSArray <NSString *> *)IvarNamesOfObject:(NSObject *)object inSuperObject:(NSObject *)superObject;
